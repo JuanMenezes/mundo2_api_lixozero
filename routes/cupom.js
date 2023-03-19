3dc32428263c4a0c46ecdf89011e968269538ae8
Juan Menezes <JUAN.MENEZES@OUTLOOK.COM>
2023-03-19 14:48:33 -0300

    Finalizacao do CRUD para as entidades do Modelo

--- routes/cupom.js
+++ routes/cupom.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var { cupom } = require('../backend_const/backend_constantes.js')
+//TODO validar não testado antes do almoço
 /* GET cupom*/
 router.get('/', function (req, res, next) {
     res.status(200).json(cupom);
+    //TODO verificar se essa logica aqui faz sentido
+    if (res.status != 200) {
+        res.status(404).send('Cupom não encontrado!');
+    }
+});
+
+
+/* GET cupom por ID */
+router.get('/:idcupom', function (req, res, next) {
+    const idcupom = req.params.idcupom;
+
+    // Procura o cupom pelo ID
+    for (let i = 0; i < cupom.length; i++) {
+        if (cupom[i].idcupom == idcupom) {
+            res.status(200).json(cupom[i]);
+            return;
+        }
+    }
+
+    // Se as cupom não for encontrado, envie um erro 404
+    res.status(404).send('cupom não encontrado!');
 });
 
 /* POST cupom */
@@ -12,5 +34,63 @@ router.post('/', function (req, res, next) {
     cupom.push(novo_cupom);
     res.status(201).send('Cupom Cadastrado!');
 });
+
+/* PUT cupons/:idcupom */
+router.put('/:idcupom', function (req, res) {
+    const idcupom = req.params.idcupom;
+    const novo_cupom = req.body;
+
+    // Encontre a Cupom pelo ID e atualize suas informações
+    for (let i = 0; i < cupom.length; i++) {
+        if (cupom[i].idcupom == idcupom) {
+            cupom[i] = novo_cupom;
+            res.status(200).send('Cupom atualizado com sucesso!');
+            return;
+        }
+    }
+
+    // Se a Cupom não for encontrado, envie um erro 404
+    res.status(404).send('Cupom não encontrado!');
+});
+
+/* PATCH users/:idcupom */
+router.patch('/:idcupom', function (req, res) {
+    const idcupom = req.params.idcupom;
+    const novo_cupom = req.body;
+
+    // Encontre a Cupom pelo ID e atualize suas informações
+    for (let i = 0; i < cupom.length; i++) {
+        if (cupom[i].idcupom == idcupom) {
+            //Não curto um n² de complexidade para algo tão simples, rever se possivel
+            for (let key in novo_cupom) {
+                cupom[i][key] = novo_cupom[key];
+            }
+            res.status(200).send('Cupom atualizado com sucesso!');
+            return;
+        }
+    }
+    // Se a Cupom não for encontrado, envie um erro 404
+    res.status(404).send('Cupom não encontrado!');
+});
+
+
+/* DELETE users/:idcupom */
+router.delete('/:idcupom', function (req, res, next) {
+    const idcupom = req.params.idcupom;
+
+    // Encontre a Cupom pelo ID e remova-o
+    for (let i = 0; i < cupom.length; i++) {
+        if (cupom[i].idcupom == idcupom) {
+            cupom.splice(i, 1);
+            res.status(200).send('Cupom removido com sucesso!');
+            return;
+        }
+    }
+
+    // Se a Cupom não for encontrado, envie um erro 404
+    res.status(404).send('Cupom não encontrado!');
+});
+
+
 
 module.exports = router;
--- routes/missoes.js
+++ routes/missoes.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var { missoes } = require('../backend_const/backend_constantes.js')
+//TODO validar não testado antes do almoço
 /* GET missoes */
 router.get('/', function (req, res, next) {
     res.status(200).json(missoes);
+    //TODO verificar se essa logica aqui faz sentido
+    if (res.status != 200) {
+        res.status(404).send('Missoes não encontrada!');
+    }
 });
+
+/* GET missoes por ID */
+router.get('/:idmissoes', function (req, res, next) {
+    const idmissoes = req.params.idmissoes;
+
+    // Procura o missoes pelo ID
+    for (let i = 0; i < missoes.length; i++) {
+        if (missoes[i].idmissoes == idmissoes) {
+            res.status(200).json(missoes[i]);
+            return;
+        }
+    }
+
+    // Se as missoes não for encontrado, envie um erro 404
+    res.status(404).send('Missoes não encontrado!');
+});
+
 
 /* POST missoes */
 router.post('/', function (req, res, next) {
@@ -12,5 +34,63 @@ router.post('/', function (req, res, next) {
     missoes.push(nova_missao);
     res.status(201).send('Missao Cadastrada');
 });
+
+
+/* PUT users/:idmissoes */
+router.put('/:idmissoes', function (req, res) {
+    const idmissoes = req.params.idmissoes;
+    const nova_missao = req.body;
+
+    // Encontre a missao pelo ID e atualize suas informações
+    for (let i = 0; i < missoes.length; i++) {
+        if (missoes[i].idmissoes == idmissoes) {
+            missoes[i] = nova_missao;
+            res.status(200).send('Missao atualizado com sucesso!');
+            return;
+        }
+    }
+
+    // Se a missao não for encontrado, envie um erro 404
+    res.status(404).send('Missao não encontrado!');
+});
+
+/* PATCH users/:idmissoes */
+router.patch('/:idmissoes', function (req, res) {
+    const idmissoes = req.params.idmissoes;
+    const nova_missao = req.body;
+
+    // Encontre a missao pelo ID e atualize suas informações
+    for (let i = 0; i < missoes.length; i++) {
+        if (missoes[i].idmissoes == idmissoes) {
+            //Não curto um n² de complexidade para algo tão simples, rever se possivel
+            for (let key in nova_missao) {
+                missoes[i][key] = nova_missao[key];
+            }
+            res.status(200).send('Missao atualizado com sucesso!');
+            return;
+        }
+    }
+    // Se a missao não for encontrado, envie um erro 404
+    res.status(404).send('Missao não encontrado!');
+});
+
+
+/* DELETE users/:idmissoes */
+router.delete('/:idmissoes', function (req, res, next) {
+    const idmissoes = req.params.idmissoes;
+
+    // Encontre a missao pelo ID e remova-o
+    for (let i = 0; i < missoes.length; i++) {
+        if (missoes[i].idmissoes == idmissoes) {
+            missoes.splice(i, 1);
+            res.status(200).send('Missao removido com sucesso!');
+            return;
+        }
+    }
+
+    // Se a missao não for encontrado, envie um erro 404
+    res.status(404).send('Missao não encontrado!');
+});
+
 
 module.exports = router;
